58e3dfaa3b197c408de44cab04c72377c7aad73f
MarcoLopez752 <marco752slayer@gmail.com>
2020-01-02 18:25:37 -0400

    InstanceLocator

---
+++ Xamarin/app/app/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using app.Models;
+
+namespace app.ViewModels
+{
+    public class MainViewModel
+    {
+        #region Properties
+        public List<Work> Listwork { get; set; }
+        #endregion
+
+        #region ViewModel
+        public WorkViewModel workViewModel { get; set; }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public List<Work> ListWork { get; internal set; }
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MainViewModel();
+            }
+            return (instance);
+        }
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            instance = this;
+            this.workViewModel = new WorkViewModel();
+        }
+    }
+}

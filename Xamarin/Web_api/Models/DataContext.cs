6f77b21f89f60e8a6b24920d85a4a31efbeb4f84
BuildTools <unconfigured@null.spigotmc.org>
2020-01-03 19:34:23 -0400

    trird commit

---
+++ Xamarin/Web_api/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace Web_api.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ Xamarin/Web_api/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_api.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Required]
+        [Range(0,100)]
+        public int nota { get; set; }
+
+    }
+}
